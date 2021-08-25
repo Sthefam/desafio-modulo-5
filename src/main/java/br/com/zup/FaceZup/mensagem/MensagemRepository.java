f35265201fd979e0004866ae01473990dafa4782
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-25 18:17:44 -0300

    Criar inteface MensagemRepository

---
+++ src/main/java/br/com/zup/FaceZup/mensagem/MensagemRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.FaceZup.mensagem;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MensagemRepository extends CrudRepository<Mensagem, Integer>{
+
+}
