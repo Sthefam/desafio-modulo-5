d9507596198a48c20a9998d5ac2d9d95ed2d1c70
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-25 19:01:27 -0300

    Criar injeção de dependencia com o modelMapper

--- src/main/java/br/com/zup/FaceZup/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/FaceZup/usuario/UsuarioController.java
@@ -1,12 +1,23 @@
 package br.com.zup.FaceZup.usuario;
 
+import br.com.zup.FaceZup.usuario.dtos.CadastrarUsuarioDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
-    @Autowired
     private UsuarioService usuarioService;
+    private ModelMapper modelMapper;
 
-
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService, ModelMapper modelMapper) {
+        this.usuarioService = usuarioService;
+        this.modelMapper = modelMapper;
+    }
 }
