7c991ca6501214b93af6f63170a1d20efcca366b
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-25 18:31:44 -0300

    Criar classe CadastrarUsuarioDTO

---
+++ src/main/java/br/com/zup/FaceZup/usuario/dtos/CadastrarUsuarioDTO.java
@@ -0,0 +1,44 @@
+package br.com.zup.FaceZup.usuario.dtos;
+
+public class CadastrarUsuarioDTO {
+
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String cargo;
+
+    public CadastrarUsuarioDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
