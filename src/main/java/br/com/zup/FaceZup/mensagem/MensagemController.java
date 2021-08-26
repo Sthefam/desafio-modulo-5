03e30d68d0853bf7e247796595860655e16143a0
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-25 22:37:44 -0300

    Criar classe MensagemController

---
+++ src/main/java/br/com/zup/FaceZup/mensagem/MensagemController.java
@@ -0,0 +1,19 @@
+package br.com.zup.FaceZup.mensagem;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chat")
+public class MensagemController {
+    private ModelMapper modelMapper;
+    private MensagemService mensagemService;
+
+    @Autowired
+    public MensagemController(ModelMapper modelMapper, MensagemService mensagemService) {
+        this.modelMapper = modelMapper;
+        this.mensagemService = mensagemService;
+    }
+}
