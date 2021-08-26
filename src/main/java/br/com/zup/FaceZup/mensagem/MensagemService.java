7e88c51e1812b1916a88d9f87299ea005e3b7990
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-25 22:34:54 -0300

    Criar método cadastrarMensagem

--- src/main/java/br/com/zup/FaceZup/mensagem/MensagemService.java
+++ src/main/java/br/com/zup/FaceZup/mensagem/MensagemService.java
@@ -1,11 +1,26 @@
 package br.com.zup.FaceZup.mensagem;
 
+import br.com.zup.FaceZup.usuario.Usuario;
+import br.com.zup.FaceZup.usuario.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MensagemService {
-    @Autowired
     private MensagemRepository mensagemRepository;
+    private UsuarioService usuarioService;
 
+    @Autowired
+    public MensagemService(MensagemRepository mensagemRepository, UsuarioService usuarioService) {
+        this.mensagemRepository = mensagemRepository;
+        this.usuarioService = usuarioService;
+    }
+
+    public Mensagem cadastrarMensagem(Mensagem mensagem){
+        Usuario usuarioDestino = usuarioService.buscarUsuarioPeloEmail(mensagem.getUsuario_destino().getEmail());
+        Usuario usuarioOrigem = usuarioService.buscarUsuarioPeloEmail(mensagem.getUsuario_origem().getEmail());
+        mensagem.setUsuario_destino(usuarioDestino);
+        mensagem.setUsuario_origem(usuarioOrigem);
+        return mensagemRepository.save(mensagem);
+    }
 }
