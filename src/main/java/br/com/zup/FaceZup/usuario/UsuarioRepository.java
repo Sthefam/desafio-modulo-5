ac56c3f3672bc8b8f92c34d933fed44c020bb790
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-25 22:24:32 -0300

    Criar método para encontrar o usuario pelo email no banco

--- src/main/java/br/com/zup/FaceZup/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/FaceZup/usuario/UsuarioRepository.java
@@ -3,5 +3,5 @@ package br.com.zup.FaceZup.usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, String>{
-
+    Usuario findUsuarioBy(String email);
 }
