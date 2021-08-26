1f1beb85242429ecc4b2ec07f345157d996a9998
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-25 22:25:29 -0300

    Criar método para buscar o usuario pelo email

--- src/main/java/br/com/zup/FaceZup/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/FaceZup/usuario/UsuarioService.java
@@ -3,10 +3,20 @@ package br.com.zup.FaceZup.usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public Usuario buscarUsuarioPeloEmail(String email){
+        Usuario usuario = usuarioRepository.findUsuarioBy(email);
+        if(usuario != null){
+            return usuario;
+        }
+        throw new RuntimeException("Usuário não encontrado!");
+    }
 
     public Usuario cadastrarUsuario(Usuario usuario){
         return usuarioRepository.save(usuario);
