98786e9baf94ff40ff8b69f4fdcf28f9f2c33fdf
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-28 12:07:32 -0300

    Criar classe para receber o statusCode e uma lista de erros

--- src/main/java/br/com/zup/FaceZup/componentes/ConfigMessage.java
+++ src/main/java/br/com/zup/FaceZup/componentes/ConfigMessage.java
@@ -1,2 +1,29 @@
-package br.com.zup.FaceZup.componentes;public class ConfigMessage {
+package br.com.zup.FaceZup.componentes;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Component
+public class ConfigMessage {
+
+    @Bean
+    public MessageSource messageResource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:erros");
+        messageSource.setDefaultEncoding("utf-8");
+
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator(){
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageResource());
+
+        return bean;
+    }
+
 }
