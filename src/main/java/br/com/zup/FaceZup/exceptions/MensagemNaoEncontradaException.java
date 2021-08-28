662f114bfbb4dd7ba6707ff9bce5c002c6ac7f3a
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-28 12:21:47 -0300

    Criar exceção personalizada para a mensagem

---
+++ src/main/java/br/com/zup/FaceZup/exceptions/MensagemNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package br.com.zup.FaceZup.exceptions;
+
+public class MensagemNaoEncontradaException extends RuntimeException {
+    public MensagemNaoEncontradaException(String message) {
+        super(message);
+    }
+}
