51910e95f1b5e355195ce2c8360b4bc8b98122f6
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-29 19:07:01 -0300

    Criar método cadastrar comentario

--- src/main/java/br/com/zup/FaceZup/comentarios/ComentarioService.java
+++ src/main/java/br/com/zup/FaceZup/comentarios/ComentarioService.java
@@ -1,14 +1,25 @@
 package br.com.zup.FaceZup.comentarios;
 
+import br.com.zup.FaceZup.usuario.Usuario;
 import br.com.zup.FaceZup.usuario.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ComentarioService {
-    @Autowired
     private UsuarioService usuarioService;
+    private ComentarioRepository comentarioRepository;
 
+    @Autowired
+    public ComentarioService(UsuarioService usuarioService, ComentarioRepository comentarioRepository) {
+        this.usuarioService = usuarioService;
+        this.comentarioRepository = comentarioRepository;
+    }
 
+    public void cadastrarComentario(Comentario comentario){
+        Usuario usuarioAutor = usuarioService.buscarUsuarioPeloEmail(comentario.getAutor().getEmail());
+
+        comentarioRepository.save(new Comentario(usuarioAutor,comentario.getTexto()));
+    }
 
 }
