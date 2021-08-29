cf69f8e039b151ebb29e285cd1bd4d74f85d9765
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-29 16:01:59 -0300

    Criar classe Post

---
+++ src/main/java/br/com/zup/FaceZup/posts/Post.java
@@ -0,0 +1,58 @@
+package br.com.zup.FaceZup.posts;
+
+import br.com.zup.FaceZup.comentarios.Comentario;
+import br.com.zup.FaceZup.usuario.Usuario;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "usuario_autor",nullable = false)
+    private Usuario usuarioAutor;
+    @Column(nullable = false)
+    private String texto;
+
+    @OneToMany(mappedBy = "usuarioAutor")
+    private List<Comentario> comentarios;
+
+    public Post() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuarioAutor() {
+        return usuarioAutor;
+    }
+
+    public void setUsuarioAutor(Usuario usuarioAutor) {
+        this.usuarioAutor = usuarioAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public List<Comentario> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
+}
