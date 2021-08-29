ab4954bdd411e4b90b08ad61aa49806ef663b8dc
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-29 16:08:30 -0300

    Criar classe PostService

---
+++ src/main/java/br/com/zup/FaceZup/posts/PostService.java
@@ -0,0 +1,12 @@
+package br.com.zup.FaceZup.posts;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    private PostRepository postRepository;
+
+
+
+}
