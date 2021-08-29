9f4691af1e093bc3a7a223a3d3b2f671b4c7b6dc
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-29 19:00:27 -0300

    Criar classe ComentarioRepository

---
+++ src/main/java/br/com/zup/FaceZup/comentarios/ComentarioRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.FaceZup.comentarios;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComentarioRepository extends CrudRepository<Comentario, Integer> {
+}
