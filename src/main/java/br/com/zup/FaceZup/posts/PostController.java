795c24067accc517ba4e09beac508d7a9d7c0db4
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-08-29 16:17:16 -0300

    Criar método cadastrarPost

--- src/main/java/br/com/zup/FaceZup/posts/PostController.java
+++ src/main/java/br/com/zup/FaceZup/posts/PostController.java
@@ -1,8 +1,15 @@
 package br.com.zup.FaceZup.posts;
 
+import br.com.zup.FaceZup.posts.dtos.CadastrarPostDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class PostController {
@@ -14,4 +21,12 @@ public class PostController {
         this.modelMapper = modelMapper;
         this.postService = postService;
     }
+
+    @PostMapping("/post")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarPost(@RequestBody @Valid CadastrarPostDTO postDTO){
+        postService.cadastrarPost(modelMapper.map(postDTO,Post.class));
+    }
+
+
 }
